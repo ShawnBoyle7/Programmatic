4bd965bb15e8843b053a719f71fb81633002b100
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-09-11 17:26:48 -0600

    implemented login modal

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import LoginFormModal from './LoginFormModal';
+import SignUpFormModal from './SignUpFormModal';
 
 const NavBar = () => {
   return (
@@ -19,9 +20,10 @@ const NavBar = () => {
           </NavLink> */}
         </li>
         <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
+          <SignUpFormModal />
+          {/* <NavLink to='/sign-up' exact={true} activeClassName='active'>
             Sign Up
-          </NavLink>
+          </NavLink> */}
         </li>
         <li>
           <NavLink to='/users' exact={true} activeClassName='active'>
