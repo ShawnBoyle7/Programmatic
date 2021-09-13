a05e3056faeb1d8cd93e1a502bc9f692fd48f432
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-12 22:51:04 -0600

    finished initializing slices of state

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import curriculum from './curriculum'
+import comments from './comments'
 
 const rootReducer = combineReducers({
   session,
+  curriculum,
+  comments
 });
 
 
