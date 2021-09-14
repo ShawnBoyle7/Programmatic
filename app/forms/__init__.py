00581e60f96838bdd6d6159188a7c7df704cca6a
Moiz Ahmad <moiznahmad@gmail.com>
2021-09-13 20:46:44 -0700

    tested aspiration rendering

--- app/forms/__init__.py
+++ app/forms/__init__.py
@@ -3,3 +3,4 @@ from .signup_form import SignUpForm
 from .edit_user_form import EditUserForm
 from .comment_form import CommentForm
 from .vote_form import VoteForm
+from .aspiration_form import AspirationForm
