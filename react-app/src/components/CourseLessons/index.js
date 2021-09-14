2e6dea9b802e6b205665bad8c94250d74f058b5b
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-09-13 22:50:05 -0600

    rendered add to learning path button, rendered lessons for indv courses

--- react-app/src/components/CourseLessons/index.js
+++ react-app/src/components/CourseLessons/index.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, useHistory } from "react-router-dom";
 
 function CourseLessons() {
     let { courseId } = useParams()
     const allLessons = Object.values(useSelector(state => state.curriculum.lessons))
-
+    const history = useHistory();
+    
+    const addToLearningPath = (_e) => {
+        dispatch(addToPath(lesson.id));
+    }
     const courseLessons = allLessons.map((lesson) => {
         return (
             <li key={lesson.id}>
                 <Link to={`/lessons/${lesson.id}`}>{allLessons.find(lesson => lesson.courseId === courseId).name}</Link>
+                <button onClick={addToLearningPath}>Add to Learning Path</button>
             </li>
         );
     });
