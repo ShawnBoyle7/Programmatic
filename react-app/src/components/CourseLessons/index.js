9bb376ad2437dee3c939e1b62a7c2042e7bebc11
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-09-13 22:19:28 -0600

    rendered all lessons for an individual course

--- react-app/src/components/CourseLessons/index.js
+++ react-app/src/components/CourseLessons/index.js
@@ -1,15 +1,23 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import { Link, useParams } from "react-router-dom";
 
 function CourseLessons() {
     let { courseId } = useParams()
+    const allLessons = Object.values(useSelector(state => state.curriculum.lessons))
+
+    const courseLessons = allLessons.map((lesson) => {
+        return (
+            <li key={lesson.id}>
+                <Link to={`/lessons/${lesson.id}`}>{allLessons.find(lesson => lesson.courseId === courseId).name}</Link>
+            </li>
+        );
+    });
     return (
         <>
             <h1>Specific Course {courseId}</h1>
             <ul>
-                <li><Link to='/lessons/1'>Course Lesson 1</Link></li>
-                <li><Link to='/lessons/2'>Course Lesson 2</Link></li>
-                <li><Link to='/lessons/3'>Course Lesson 3</Link></li>
+                {courseLessons}
             </ul>
         </>
     )
