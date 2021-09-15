9b542146e6001fc127acb27f6b8981a03e7e8191
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-14 22:14:25 -0600

    delete user modal done

--- react-app/src/components/Profile/index.js
+++ react-app/src/components/Profile/index.js
@@ -1,25 +1,9 @@
-import React, { useState } from "react";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
+import React from "react";
 import EditUserFormModal from "../EditUserFormModal";
 import LearningPath from "../LearningPath";
-import { deleteUser } from "../../store/session";
 import DeleteUserModal from "../DeleteUserModal";
 
 function Profile({ sessionUser }) {
-    const dispatch = useDispatch()
-    const history = useHistory()
-    const [showDelete, setShowDelete] = useState(false)
-
-    const renderDelete = () => {
-        setShowDelete(true)
-    };
-
-    const handleDelete = () => {
-        dispatch(deleteUser(sessionUser.id))
-        setShowDelete(false)
-        history.push("/")
-    };
 
     return (
             sessionUser &&
@@ -28,10 +12,6 @@ function Profile({ sessionUser }) {
                 <LearningPath />
                 <EditUserFormModal/> 
                 <DeleteUserModal/>
-                <button onClick={renderDelete}>Deactivate Account</button>
-
-                {showDelete &&
-                <button onClick={handleDelete}>Confirm Deactivation</button>}
             </>
     )
 }
