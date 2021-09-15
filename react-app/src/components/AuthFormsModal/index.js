c36e8d506b890af215810fcdc4b01a176f9ea21a
Moiz Ahmad <moiznahmad@gmail.com>
2021-09-14 19:28:44 -0700

    fixed auth modal close bug where URL reflects inaccessible page

--- react-app/src/components/AuthFormsModal/index.js
+++ react-app/src/components/AuthFormsModal/index.js
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom'
 import { Modal } from '../../context/Modal';
 import LoginForm from '../auth/LoginForm';
 import SignUpForm from '../auth/SignUpForm';
 
 function AuthFormsModal() {
     const [showModal, setShowModal] = useState(true);
+    const history = useHistory();
+
+    const handleClose = () => {
+        setShowModal(false);
+        history.goBack()
+    }
 
     return (
     <>
-        {/* <button onClick={() => setShowModal(true)}>All The Auth!</button> */}
         {showModal && (
-        <Modal onClose={() => setShowModal(false)}>
+        <Modal onClose={handleClose}>
             <p>You need to be logged in to access that!</p>
             <div className='auth-modal'>
                 <LoginForm setShowModal={setShowModal}/>
