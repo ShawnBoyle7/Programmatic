53a4ebf09ecb0a4b3efc24aef7e56759a0be9ca0
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-14 23:41:47 -0600

    rendered search dropdown, debugging results interactivity

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from '../auth/LogoutButton';
 import LoginFormModal from '../LoginFormModal';
 import SignUpFormModal from '../SignUpFormModal';
+import SearchDropdown from '../SearchDropdown';
 import './NavBar.css'
 
 const NavBar = ({sessionUser, authenticated}) => {
+
+    // Tracking user input in a state variable
+    const [searchQuery, setSearchQuery] = useState("");
+    // State variable for conditional rendering search results
+    const [renderSearchDropdown, setRenderSearchDropdown] = useState(false);
+
+    // Tracking user input from the state variable in a use effect because apparently you need it for real time updates
+    useEffect(() => {
+
+        // If no user input, don't render search results
+        if (!searchQuery.length) return setRenderSearchDropdown(false)
+
+        // If there is user input, we set this state variable to true so that we can render the dropdown results
+        setRenderSearchDropdown(true)
+
+        console.log(searchQuery)
+    }, [searchQuery]);
 
     return (
         <nav>
@@ -17,8 +35,18 @@ const NavBar = ({sessionUser, authenticated}) => {
                 Courses
             </NavLink>
 
-            <div>
-                <input placeholder='Search Here...'/>
+            <div className="nav-search-div">
+                <input 
+                placeholder='Search Here...'
+                // Comment this later
+                value={searchQuery}
+                // Listen to user input and update our state variable on change
+                onChange={e => setSearchQuery(e.target.value)}
+                />
+                {/* If there is user input, render the dropdown results */}
+                {renderSearchDropdown && 
+                    // Passing props for filtering our query and toggle the rendering of our dropdown results
+                    <SearchDropdown searchQuery={searchQuery} setRenderSearchDropdown={setRenderSearchDropdown}/>}
             </div>
 
             { !authenticated ? 
