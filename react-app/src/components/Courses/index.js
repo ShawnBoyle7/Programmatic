d9692058c7dbcd1686e1258046b2bc35d5729910
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-09-14 20:57:02 -0600

    implemented logic for adding entire course to learning path without duplicate lessons

--- react-app/src/components/Courses/index.js
+++ react-app/src/components/Courses/index.js
@@ -3,18 +3,40 @@ import { Route, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProtectedRoute from '../auth/ProtectedRoute';
 import CourseLessons from '../CourseLessons';
+import { addToPath } from "../../store/session"
 
 function Courses() {
     const courses = Object.values(useSelector(state => state.curriculum.courses))
+    const userId = useSelector(state => state.session.user?.id)
+    const userAspirations = useSelector(state => state.session.user.aspirations)
+    // when someone clicks the add button,
+    //need to iterate through all the lessons in the course to see if any are already part of the learning path.
+    //if so, don't add those
+    const addToLearningPath = (e) => {
+        const userAspirations =Object.values(useSelector(state => state.session.user.aspirations))
+        const allLessons = Object.values(curriculum?.lessons)
+        const courseLessons = allLessons.filter(lesson => lesson.courseId === course.id)
+        courseLessons.forEach((lesson) => {
+            if (!userAspirations.find(lesson.id)) {
+                dispatch(addToPath(e.target.id, userId, courseId));
+            }
+        })
 
+    }
     return (
     <>
         <Route exact path='/courses'>
             <div>
-                {courses.map(course =>
-                    <div key={course.id}>
-                        <Link to={`/courses/${course.id}`}>{course.name}</Link>
-                    </div>)}
+                {courses.map(course => {
+                    return(
+                      <div key={course.id}>
+                            <Link to={`/courses/${course.id}`}>{course.name}</Link>
+                            <button id={course.id} onClick={addToLearningPath}>Add to Learning Path</button>
+                        </div>
+                    )
+
+                })
+                    }
             </div>
         </Route>
         <ProtectedRoute path='/courses/:courseId'>
