623d12b8ffcf96ea0a31fd66d4be7ea171c52292
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-09-15 23:19:03 -0600

    working on learning path logic

--- react-app/src/components/LearningPath/index.js
+++ react-app/src/components/LearningPath/index.js
@@ -3,13 +3,38 @@ import AspirationsDiv from "../AspirationsDiv"
 
 function LearningPath() {
   const sessionUser = useSelector(state => state.session?.user)
-  const aspirations = sessionUser?.aspirations.sort((a, b) => {
+  const aspirationsArr = sessionUser?.aspirations.sort((a, b) => {
     return a.id - b.id
   });
 
-    const aspirationComponents = aspirations?.map((aspiration) =>
+  const getCourseIds = () => {
+    for(let i = 0; i < aspirationsArr.length; i++) {
+      let aspiration = aspirationsArr[i];
+      let courseIdArr = [];
+        //check if the course id is already in arr
+      if(!courseIdArr.includes(aspiration.courseId)){
+        courseIdArr.push(aspiration.courseId)
+      }
+    }
+    return courseIdArr;
+  }
+
+  let aspByCourseArr = []
+  courseIdArr.forEach(courseId => {
+    let allAspInCourse = aspirationsArr.filter(asp => asp.couseId === courseId)
+    aspByCourseArr.push(allAspInCourse)
+  });
+
+  
+
+  //check aspiration.courseId
+    //if haven't encountered, create a new array/div
+    //else add to array/div already created
+
+    const aspirationComponents = aspirationsArr?.map((aspiration) =>
         <div key={aspiration.id}>
-          <AspirationsDiv aspiration={aspiration} />
+          {/* <AspirationsDiv aspiration={aspiration} /> */}
+          <div>{separateByCourse}</div>
         </div>
     );
 
@@ -17,6 +42,7 @@ function LearningPath() {
     <>
       <h1>Learning Path: </h1>
       <div>{aspirationComponents}</div>
+
     </>
   );
 }
