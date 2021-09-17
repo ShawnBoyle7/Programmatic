a93386da949a631b19de5dd0d8ce5f4c5805390e
Ivy Huynh <ivy.a.huynh@gmail.com>
2021-09-17 15:43:30 -0600

    pull to main

--- react-app/src/components/CourseDiv/index.js
+++ react-app/src/components/CourseDiv/index.js
@@ -41,7 +41,7 @@ const CourseDiv = ({ course }) => {
     }
 
     return(
-        <div className="course-div div-hover" onClick={() => history.push(`/courses/${course.id}`)}>
+        <div className="course-div" onClick={() => history.push(`/courses/${course.id}`)}>
 
             <div className="course-header">
                 Course
