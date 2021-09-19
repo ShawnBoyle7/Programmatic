d79f7655003988c23789bbadc168b8c566afbdac
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-19 15:00:56 -0600

    splash styling

--- react-app/src/components/AuthFormsModal/index.js
+++ react-app/src/components/AuthFormsModal/index.js
@@ -4,20 +4,25 @@ import { Modal } from '../../context/Modal';
 import LoginForm from '../auth/LoginForm';
 import SignUpForm from '../auth/SignUpForm';
 
-function AuthFormsModal() {
-    const [showModal, setShowModal] = useState(true);
+function AuthFormsModal({ forbidden=true }) {
+    const [showModal, setShowModal] = useState(forbidden);
     const history = useHistory();
 
     const handleClose = () => {
         setShowModal(false);
-        history.goBack()
+        if (forbidden) history.goBack()
     }
 
     return (
     <>
+        {!forbidden &&
+            <button onClick={() => setShowModal(true)}>Get Started</button>
+        }
         {showModal && (
         <Modal onClose={handleClose} className="auth-modal">
-            <p className="auth-header">You need to be logged in to access that!</p>
+            { forbidden &&
+                <p className="auth-header">You need to be logged in to access that!</p>
+            }
             <div className='auth-forms'>
                 <LoginForm setShowModal={setShowModal}/>
                 <SignUpForm setShowModal={setShowModal}/>
