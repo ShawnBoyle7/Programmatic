d82249aac434f52e0186eb8ac3687818cf177adc
Shawn Boyle <shawn.boyle01@gmail.com>
2021-09-18 18:53:23 -0600

    mostly header changes and centering

--- react-app/src/components/Home/index.js
+++ react-app/src/components/Home/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import Splash from "../Splash";
 import { useSelector } from 'react-redux'
 import LessonDiv from "../LessonDiv";
+import "./Home.css"
 
 function Home({authenticated}) {
     const lessons = Object.values(useSelector(state => state.curriculum.lessons))
@@ -15,7 +16,7 @@ function Home({authenticated}) {
         authenticated ? 
         <>
             <h1 className="popular-lessons-header">
-                Browse Popular Lessons!
+                Most Popular Lessons
             </h1>
             <div className="popular-lessons">
                 {lessonsByPopularity.map(lesson => 
