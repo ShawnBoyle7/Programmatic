68c577f8629912c5e44b0054cb08df619cf04194
Shawn Boyle <shawn.boyle01@gmail.com>
2021-10-18 14:57:53 -0600

    metadata

--- app/api/utils.py
+++ app/api/utils.py
@@ -1,7 +1,4 @@
 def validation_errors_to_error_messages(validation_errors):
-    """
-    Simple function that turns the WTForms validation errors into a simple list
-    """
     errorMessages = []
     for field in validation_errors:
         for error in validation_errors[field]:
